ac70c7e4b8a5ae721ea7d641bb840429f638c768
спс никитос <163382818+semyonov1511@users.noreply.github.com>
2024-05-14 11:24:07 +0300

    Actions classes created

---
+++ lab5/src/main/java/Particular_Actions/Hit.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Particular_Actions;
+
+/**
+ *
+ * @author semyo
+ */
+public class Hit {
+    
+}
