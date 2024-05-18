4acaadecfea7e45757835baa8974880c812c7ef0
спс никитос <163382818+semyonov1511@users.noreply.github.com>
2024-05-18 16:16:12 +0300

    Class main changes for music

--- lab5/src/main/java/com/mycompany/lab5/Lab5.java
+++ lab5/src/main/java/com/mycompany/lab5/Lab5.java
@@ -1,11 +1,33 @@
 package com.mycompany.lab5;
 
 import Interface.JFrames;
+import java.io.File;
+import java.io.IOException;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class Lab5 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws LineUnavailableException, UnsupportedAudioFileException {
+
+        System.out.println("lab");
         JFrames app = new JFrames();
         app.setVisible(true);
+        try {
+            File soundFile = new File("crab.wav");
+            AudioInputStream ais = AudioSystem.getAudioInputStream(soundFile);
+            try (
+                    Clip clip = AudioSystem.getClip()) {
+                clip.open(ais);
+                clip.setFramePosition(0);
+                clip.start();
+                Thread.sleep(clip.getMicrosecondLength() / 1000);
+                clip.stop();
+            }
+        } catch (IOException | UnsupportedAudioFileException | LineUnavailableException | InterruptedException exc) {
+        }
     }
 }
